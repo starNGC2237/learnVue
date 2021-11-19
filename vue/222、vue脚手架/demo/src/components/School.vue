120d45117b3c7cf9ac44e433c6b047af15e03771
starNGC2237 <2236992924@qq.com>
2021-11-20 01:22:59 +0800

    63

---
+++ vue/222、vue脚手架/demo/src/components/School.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class='demo'>
+        <h2>学校位置：{{address}}</h2>
+        <h2>学校名称：{{schoolName}}</h2>
+    </div>
+</template>
+<script>
+    export default {
+        name:'School',
+        template:'<div><h2>学校位置：{{address}}</h2><h2>学校名称：{{schoolName}}</h2></div>',
+        data(){
+            return {
+                address:'昌平',
+                schoolName:'尚硅谷'
+            }
+        }
+    }
+</script>
+
+<style>
+    /**
+    DAsAS
+    */
+    .demo{
+        background-color: orange;
+    }
+</style>
+
--- vue/222、vue脚手架/demo/src/main.js
+++ vue/222、vue脚手架/demo/src/main.js
@@ -1,8 +1,11 @@
+//引入vue
 import Vue from 'vue'
 import App from './App.vue'
 
+//关闭提示
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+//挂载app
