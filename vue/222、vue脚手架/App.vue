4200a19c82cda597a5593be560f59b5c4b2e3557
starNGC2237 <2236992924@qq.com>
2021-11-19 23:10:23 +0800

    62

---
+++ vue/222、vue脚手架/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Student></Student>
+        <School></School>
+    </div>
+</template>
+
+<script>
+import School from './School.vue';
+import Student from './Student.vue'
+export default {
+    name:'App',
+    components:{
+        Student,
+        School
+    }
+}
+</script>
---
+++ vue/222、vue脚手架/Student.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>学生年龄：{{age}}</h2>
+        <h2>学生名称：{{studentName}}</h2>
+        <hr>
+        <button @click="age++">实打实的</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return {
+            age:18,
+            studentName:'张三'
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
