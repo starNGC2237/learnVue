f14936e59d96eb492c5e42a37181ea123577b4e6
starNGC2237 <starmars_lu@163.com>
2022-01-16 20:03:35 +0800

    2

--- vue/777、VueRouter/vue_router_demo/src/pages/About.vue
+++ vue/777、VueRouter/vue_router_demo/src/pages/About.vue
@@ -2,9 +2,9 @@
  * @Author: zhilutianji
  * @Date: 2021-12-20 23:39:17
  * @LastEditors: zhilutianji
- * @LastEditTime: 2021-12-20 23:42:36
+ * @LastEditTime: 2021-12-23 21:02:11
  * @Description: file content
- * @FilePath: \vue_router_demo\src\components\About.vue
+ * @FilePath: \vue_router_demo\src\pages\About.vue
 -->
 <template>
   <h2>我是About的内容</h2>
@@ -12,7 +12,17 @@
 
 <script>
 export default {
-    name:'About'
+    name:'About',
+    //进入守卫，通过路由规则，进入该组件时被调用
+    beforeRouteEnter (to, from, next) {
+      console.log(to,from,next)
+      next()
+    },
+    //离开守卫，通过路由规则，离开该组件时被调用
+    beforeRouteLeave (to, from, next) {
+      console.log(to,from,next)
+      next()
+    }
 }
 </script>
 
